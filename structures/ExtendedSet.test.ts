fe75d47afa8441bb092d11780d183802c334a5f9
Grant Sander <gksander93@gmail.com>
2020-02-08 11:32:43 -0700

    Sets

---
+++ structures/ExtendedSet.test.ts
@@ -0,0 +1,80 @@
+import { ExtendedSet } from "./ExtendedSet";
+
+let Odds = new ExtendedSet<number>([]);
+let Evens = new ExtendedSet<number>([]);
+let Primes = new ExtendedSet<number>([]);
+let Empty = new ExtendedSet<number>([]);
+
+/**
+ * Extended set
+ */
+describe("ExtendedSet", () => {
+  // Setup
+  beforeEach(() => {
+    Odds = new ExtendedSet<number>([1, 3, 5, 7, 9]);
+    Evens = new ExtendedSet<number>([0, 2, 4, 6, 8, 10]);
+    Primes = new ExtendedSet<number>([2, 3, 5, 7]);
+    Empty = new ExtendedSet<number>([]);
+  });
+
+  // Unions
+  describe("ExtendedSet#Union", () => {
+    test("Odd Union Even", () => {
+      const U = ExtendedSet.Union(Odds, Evens);
+      expect(Array.from(U).sort()).toEqual(
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].sort(),
+      );
+    });
+
+    test("Even Union Prime", () => {
+      const U = ExtendedSet.Union(Evens, Primes);
+      expect(Array.from(U).sort()).toEqual([0, 2, 3, 4, 5, 6, 7, 8, 10].sort());
+    });
+
+    test("Odds union Empty", () => {
+      const U = ExtendedSet.Union(Odds, Empty);
+      expect(Array.from(U).sort()).toEqual(Array.from(Odds).sort());
+    });
+  });
+
+  // Intersection
+  describe("ExtendedSet#Intersection", () => {
+    test("Even intersect Odd", () => {
+      const I = ExtendedSet.Intersection(Evens, Odds);
+      expect(Array.from(I)).toEqual([]);
+    });
+
+    test("Evens intersect Primes", () => {
+      const I = ExtendedSet.Intersection(Evens, Primes);
+      expect(Array.from(I).sort()).toEqual([2].sort());
+    });
+
+    test("Odds intersect Primes", () => {
+      const I = ExtendedSet.Intersection(Odds, Primes);
+      expect(Array.from(I).sort()).toEqual([3, 5, 7].sort());
+    });
+
+    test("Evens intersect Empty", () => {
+      const I = ExtendedSet.Intersection(Evens, Empty);
+      expect(Array.from(I).sort()).toEqual([].sort());
+    });
+  });
+
+  // Difference
+  describe("ExtendedSet#Difference", () => {
+    test("Evens minus Odds", () => {
+      const D = ExtendedSet.Difference(Evens, Odds);
+      expect(Array.from(D).sort()).toEqual(Array.from(Evens).sort());
+    });
+
+    test("Evens minus Primes", () => {
+      const D = ExtendedSet.Difference(Evens, Primes);
+      expect(Array.from(D).sort()).toEqual([0, 4, 6, 8, 10].sort());
+    });
+
+    test("Primes minus Odds", () => {
+      const D = ExtendedSet.Difference(Primes, Odds);
+      expect(Array.from(D).sort()).toEqual([2].sort());
+    });
+  });
+});
---
+++ structures/ExtendedSet.ts
@@ -0,0 +1,37 @@
+/**
+ * Modified Set class with set operations
+ */
+export class ExtendedSet<T> extends Set<T> {
+  // Constructor
+  constructor(values: T[]) {
+    super(values);
+  }
+
+  // Union
+  static Union<T>(A: ExtendedSet<T>, B: ExtendedSet<T>): ExtendedSet<T> {
+    return new ExtendedSet<T>([...A.values(), ...B.values()]);
+  }
+
+  // Intersection
+  static Intersection<T>(A: ExtendedSet<T>, B: ExtendedSet<T>): ExtendedSet<T> {
+    const intersection = new ExtendedSet<T>([]);
+    for (let val of A) {
+      if (B.has(val)) intersection.add(val);
+    }
+    return intersection;
+  }
+
+  // Difference
+  static Difference<T>(A: ExtendedSet<T>, B: ExtendedSet<T>): ExtendedSet<T> {
+    const diffed = new ExtendedSet<T>([]);
+    for (let val of A) {
+      // If B doesn't contain val, add it to our difference.
+      if (!B.has(val)) {
+        diffed.add(val);
+      }
+    }
+    return diffed;
+  }
+
+  // TODO: Subset
+}
